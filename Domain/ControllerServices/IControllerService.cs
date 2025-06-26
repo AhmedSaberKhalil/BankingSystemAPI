6120f8b45a06988cce6019f4c5be5cfb999e217d
unknown <ahmedsaberacu77@gmail.com>
2025-06-26 14:44:46 +0300

    Refactor NewAccountController: apply SOLID principles by adding IConrollerService interface

---
+++ Domain/ControllerServices/IControllerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DTOs.DtoModels;
+using Domain.Models;
+
+namespace Domain.ControllerServices
+{
+    public interface IControllerService<T> where T : class
+    {
+      
+        Task<Result<IEnumerable<T>>> GetAllAsync();
+        Task<Result<T>> GetByIdAsync(int id);
+        Task<Result<T>> AddAsync(T entity);
+        Task<Result<bool>> UpdateAsync(int id, T entity);
+        Task<Result<bool>> DeleteAsync(int id);
+
+    }
+}
